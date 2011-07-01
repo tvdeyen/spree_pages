3ee8c8962cbf2ebb096568e0808a855acaa5ce83
Thomas von Deyen <tvdeyen@gmail.com>
2011-07-02 00:53:30 +0200

    Names for Deface overrides

--- lib/spree_pages_hooks.rb
+++ lib/spree_pages_hooks.rb
@@ -2,6 +2,7 @@ class SpreePagesHooks < Spree::ThemeSupport::HookListener
   
   Deface::Override.new(
     :virtual_path => "layouts/admin",
+    :name => 'pages_admin_tab',
     :insert_bottom => "[data-hook='admin_tabs']",
     :text => %(<%= tab(:pages) %>)
   )
